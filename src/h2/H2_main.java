c889bd11a2c989278615fa2d8084df14796a1f00
Felix Hattrup <felix@MacBook-Air-von-Felix.local>
2025-10-28 11:09:35 +0100

    2. Aufgabe

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,4 +1,28 @@
 package h2;
 
 public class H2_main {
+    public static void main() {
+        int i = 20, j = 8, k = 3, min = 4, max = 5;
+
+        // min finden
+        if (i < j && i<k) {
+            min = i;
+        }
+        else if (j < i && j < k) {
+            min = j;
+        }
+        else if (k < i && k < j) {
+            min = k;
+        }
+
+        // max finden
+        if (i > j && i > k){
+            max = i;
+        } else if (j > i && j > k) {
+            max = j;
+        } else if (k > i && k > j) {
+            max = k;
+        }
+        System.out.println("Max: " + max + " Min: " + min);
+    }
 }
